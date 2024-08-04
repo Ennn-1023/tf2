45d1cf03fca80189146b3639806c809911442a2f
Kevin171717 <kevin47338647@gmail.com>
2024-08-04 19:43:48 +0800

    copy and define
    copy method to model.py and define conv

--- Hifill-tensorflow/model/layers.py
+++ Hifill-tensorflow/model/layers.py
@@ -1,7 +1,19 @@
 from tensorflow import keras
 import tensorflow as tf
+from tensorflow.keras import layers
 
 
+def gen_conv(x, cnum, ksize, stride=1, rate=1, name='conv',
+             padding='SAME', activation=tf.nn.elu, training=True, dtype=tf.float32):
+    x = conv2d(x, cnum, ksize, stride, dilation_rate=rate,
+        activation=activation, padding=padding, name=name, dtype=dtype)
+    return x
+
+def gen_deconv(x, cnum, name='upsample', padding='SAME', training=True, dtype=tf.float32):
+    x = tf.image.resize(x, size=[x.shape[1] * 2, x.shape[2] * 2], method='bilinear')
+    x = gen_conv(x, cnum, 3, 1, name=name+'_conv', padding=padding,
+            training=training, dtype=dtype)
+    return x
 def conv2d(x, output_dim, ksize, stride, dilation_rate=1, activation=None, padding='same', name='conv',
            dtype=tf.float32):
     # 使用 Keras 的 Conv2D 層來替代自定義的卷積操作
@@ -21,6 +33,79 @@ def conv2d(x, output_dim, ksize, stride, dilation_rate=1, activation=None, paddi
     # 將輸入 x 通過 Conv2D 層進行卷積操作
     conv = conv_layer(x)
     return conv
+
+def gen_conv_gated(x, cnum, ksize, stride=1, rate=1, name='conv',
+             padding='SAME', slim=True, activation=None, training=True, dtype=tf.float32):
+    x1 = conv2d(x, cnum, ksize, stride, dilation_rate=rate,
+        activation=None, padding=padding, name=name+'_feat', dtype=dtype)
+    x2 = conv2d(x, cnum, ksize, stride, dilation_rate=rate,
+        activation=None, padding=padding, name=name+'_gate', dtype=dtype)
+    #x1, x2 = tf.split(x, 2, axis=3)
+    x = tf.sigmoid(x2) * tf.nn.elu(x1)
+    return x
+
+def conv2d_ds(x, output_dim, ksize, stride, dilation_rate=1, activation=None, \
+                        padding='SAME', name='conv', dtype=tf.float32):
+    # 使用 Keras 的 SeparableConv2D 進行深度可分離卷積
+    conv_layer = layers.SeparableConv2D(
+        filters=output_dim,
+        kernel_size=ksize,
+        strides=stride,
+        dilation_rate=dilation_rate,
+        padding=padding,
+        activation=activation,
+        depthwise_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.05),
+        pointwise_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.05),
+        bias_initializer=tf.keras.initializers.Constant(0.0),
+        name=name,
+        dtype=dtype
+    )
+
+    # 將輸入 x 通過 SeparableConv2D 層進行卷積操作
+    y = conv_layer(x)
+    return y
+
+def gen_deconv_gated_ds(x, cnum, name='upsample', padding='SAME', training=True, dtype=tf.float32):
+    x = tf.image.resize(x, size=[x.shape[1] * 2, x.shape[2] * 2], method='bilinear')
+    #x = resize(x, func=tf.compat.v1.image.resize_bilinear)
+    x = gen_conv_gated_ds( x, cnum, 3, 1, name=name+'_conv', padding=padding,
+        training=training, dtype=dtype)
+    return x
+
+def gen_conv_gated_ds(x, cnum, ksize, stride=1, rate=1, name='conv',
+             padding='SAME', slim=True, activation=None, training=True, dtype=tf.float32):
+    x1 = conv2d(x, cnum, ksize, stride, dilation_rate=rate,
+        activation=None, padding=padding, name=name, dtype=dtype)
+    x2 = conv2d_ds(x, cnum, 3, stride, dilation_rate=1,
+        activation=None, padding=padding, name=name, dtype=dtype)
+    x = tf.sigmoid(x2) * tf.nn.elu(x1)
+    return x
+
+def gen_conv_gated_slice(x, cnum, ksize, stride=1, rate=1, name='conv',
+             padding='SAME', slim=True, activation=None, training=True, dtype=tf.float32):
+    x1 = conv2d(x, cnum, ksize, stride, dilation_rate=rate,
+        activation=None, padding=padding, name=name+'_feat', dtype=dtype)
+    x2 = conv2d(x, 1, ksize, stride, dilation_rate=rate,
+        activation=None, padding=padding, name=name+'_gate', dtype=dtype)
+    #x1, x2 = tf.split(x, [cnum,1], axis=3)
+    x = tf.sigmoid(x2) * tf.nn.elu(x1)
+    return x
+
+def gen_deconv_gated_slice(x, cnum, name='upsample', padding='SAME', training=True, dtype=tf.float32):
+    #with tf.compat.v1.variable_scope(name):
+        #x = resize(x, func=tf.compat.v1.image.resize_bilinear)
+    x = tf.image.resize(x, size=[x.shape[1] * 2, x.shape[2] * 2], method='bilinear')
+    x = gen_conv_gated_slice(  x, cnum, 3, 1, name=name+'_conv', padding=padding,
+        training=training, dtype=dtype)
+    return x
+
+def gen_deconv_gated(x, cnum, name='upsample', padding='SAME', training=True, dtype=tf.float32):
+    x = tf.image.resize(x, size=[x.shape[1] * 2, x.shape[2] * 2], method='bilinear')
+    #with tf.variable_scope(name):
+    #   x = resize(x, func=tf.image.resize_bilinear)
+    x = gen_conv_gated( x, cnum, 3, 1, name=name+'_conv', padding=padding,
+        training=training, activation=None, dtype=dtype)
+    return x
 
 def dis_conv(x, cnum, ksize=5, stride=2, name='conv', dtype=tf.float32):
     x = conv2d(x, cnum, ksize, stride, padding='SAME', name=name, dtype=dtype,activation='leaky_relu')
--- Hifill-tensorflow/model/model.py
+++ Hifill-tensorflow/model/model.py
@@ -1,5 +1,28 @@
+
+
 import tensorflow as tf
-from layers import Discriminator_block
+from layers import Discriminator_block, gen_conv, gen_deconv, gen_deconv_gated, gen_conv_gated, gen_conv_gated_ds, \
+    gen_deconv_gated_ds, gen_conv_gated_slice, gen_deconv_gated_slice
+
+
+def get_conv_op(conv_type):
+    #gen_conv = ops.GenConvGated()
+    #print(conv_type, 'ds')
+    if conv_type == 'none':
+        conv = gen_conv
+        deconv = gen_deconv
+    elif conv_type == 'regular':
+        conv = gen_conv_gated
+        deconv = gen_deconv_gated
+    elif conv_type == 'ds':
+        conv = gen_conv_gated_ds
+        deconv = gen_deconv_gated_ds
+    elif conv_type == 'slice':
+        conv = gen_conv_gated_slice
+        deconv = gen_deconv_gated_slice
+    else:
+        raise('wrong conv type ' + conv_type)
+    return conv, deconv
 
 # Our model class
 class MyModel:
@@ -8,7 +31,7 @@ class MyModel:
         self.generator = self.build_generator()
         self.discriminator = self.build_discriminator()
 
-    def build_generator(self, img_input, mask_input):
+    def build_generator(self, img_input, mask_input, config=None):
         """
         Build the generator model
         """
@@ -18,6 +41,8 @@ class MyModel:
 
         # add masked image
         x = tf.concat([img_input, mask_input], axis=3) # (512, 512, 4)
+        # conv and deconv for stage-2
+        conv2, deconv2 = get_conv_op(config.REFINE_CONV_TYPE)
 
         # sample output
         output = tf.keras.layers.Conv2D(3, (3, 3), padding='same')(x)
